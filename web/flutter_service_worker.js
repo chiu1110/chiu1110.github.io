805498737c0d7e1122291f286b6562ec4d3161e9
Eric Chiu <chiu1110@gmail.com>
2023-02-20 06:50:03 -0800

    fix

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -290,9 +290,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "50bd8ab035fcebc28c26843e07b342bb",
 "icons/Icon-maskable-192.png": "9f7a76ba6afe56562021eea4fed54e82",
 "icons/Icon-maskable-512.png": "50bd8ab035fcebc28c26843e07b342bb",
-"index.html": "8a22f8eee6c92d0f3448ea66effb848a",
-"/": "8a22f8eee6c92d0f3448ea66effb848a",
-"main.dart.js": "f7b7127006a35a6f21c1f278c166b53e",
+"index.html": "da19a7fd97929b2b58c315aa3936bf3c",
+"/": "da19a7fd97929b2b58c315aa3936bf3c",
+"main.dart.js": "850150345ae5b8248d3f752a7561c2a3",
 "manifest.json": "088543546a206bc1e25c803d0160fed6",
 "version.json": "128d76a3319529000c3d1f7a33ca3ded"
 };
